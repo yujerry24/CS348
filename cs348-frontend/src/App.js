01cdcbe49fd2d04a01b86aa472841ed25b88e149
Thomas Cheng <35661641+Androxium@users.noreply.github.com>
2020-06-03 16:23:03 -0400

    Table with dummy data (#4)

--- cs348-frontend/src/App.js
+++ cs348-frontend/src/App.js
@@ -1,26 +1,81 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
+import DataTable from './ui/DataTable';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const headings = [
+  'Artist',
+  'Title',
+  'Year'
+];
+
+class App extends Component {
+  render() {
+
+    const headings = [
+      'Product name',
+      'SKU',
+      'Stock quantity',
+      'Wholesale cost',
+      'Sale price',
+      'Quantity sold',
+      'Gross sales',
+      'Net sales',
+      'Notes',
+    ];
+
+    const rows = [
+      [
+        'Red and black plaid scarf with thin red stripes and thick black stripes',
+        124689325,
+        28,
+        '$35.00',
+        '$60.00',
+        12,
+        '$720.00',
+        '$300.00',
+        '',
+      ],
+      [
+        'Yellow plaid scarf',
+        124689389,
+        0,
+        '$35.00',
+        '$60.00',
+        20,
+        '$1200.00',
+        '$500.00',
+        'Currently on back order by the supplier. Do not place another order to restock.',
+      ],
+      [
+        'Blue plaid scarf',
+        124689332,
+        30,
+        '$35.00',
+        '$60.00',
+        10,
+        '$600.00',
+        '$250.00',
+        '',
+      ],
+      [
+        'Pink plaid scarf',
+        124689376,
+        16,
+        '$35.00',
+        '$60.00',
+        4,
+        '$240.00',
+        '$100.00',
+        '',
+      ],
+    ];
+
+    return (
+      <div>
+        <DataTable headings={headings} rows={rows} />
+      </div>
+    );
+  }
 }
 
 export default App;
