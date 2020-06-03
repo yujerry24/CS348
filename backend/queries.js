e4c17e13ffe3617dbba619db120aa6fe04d5b157
yujerry24 <yujerry24@gmail.com>
2020-06-03 17:00:21 -0400

    REEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEE

--- backend/queries.js
+++ backend/queries.js
@@ -1,10 +1,11 @@
+require('dotenv').config()
 const Pool = require('pg').Pool
 const pool = new Pool({
-  user: 'postgres',
-  host: '35.237.58.41',
-  database: 'postgres',
-  password: 'cs348',
-  port: 5432,
+  user: process.env.POSTGRES_USER,
+  host: process.env.POSTGRES_HOST,
+  database: process.env.POSTGRES_DATABASE,
+  password: process.env.POSTGRES_PASSWORD,
+  port: process.env.POSTGRES_PORT,
 })
 
 const getPlaylist1 = (request, response) => {
