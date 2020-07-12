7b72a104e519f2081e44811118b9cbdab9e20c29
Thomas Cheng <thjcheng@uwaterloo.ca>
2020-07-12 13:09:09 -0400

    wip - setting up endpoint

---
+++ endpointImpl/mostPopQueries.js
@@ -0,0 +1,72 @@
+const { pool } = require('../dbPool')
+
+/*
+    GET
+    /playlist/mostPopularSongs
+*/
+const getTop20Songs = (req, response) => {
+    pool.query(
+        `
+            SELECT S.song_id, S.name as song_name, video_duration, Al.name as album_name, A.name as artist_name 
+            FROM song S 
+                INNER JOIN wrote W on S.song_id = W.song_id
+                INNER JOIN artist A on W.artist_id = A.artist_id
+                INNER JOIN album Al ON S.album_id = Al.album_id
+            WHERE(
+                S.song_id in (
+                    SELECT * FROM (
+                        SELECT P.song_id FROM in_playlist P
+                        GROUP BY P.song_id
+                        ORDER BY count(P.song_id) DESC
+                    ) AS in_play LIMIT 20
+                )
+            );
+        `
+    )
+    .then(results => { response.status(200).json(results.rows) })
+    .catch(error => {
+        console.log(error.detail);
+        response.status(400).json(error.detail);
+    })
+}
+
+/*
+    GET
+    /playlist/mostPopularArtists
+*/
+const getTop20Artists = (req, response) => {
+    pool.query(
+        `
+            SELECT DISTINCT * FROM (
+                SELECT A.name AS artist_name, inPlay.in_num_of_playlists
+                FROM artist A
+                    INNER JOIN wrote W ON A.artist_id = W.artist_id
+                    INNER JOIN song S ON S.song_id = W.song_id
+                    INNER JOIN (
+                        -- get the most popular artist in terms of number of playlists they are in
+                        SELECT * FROM (
+                            SELECT A2.artist_id, count(A2.artist_id) AS in_num_of_playlists
+                            FROM (
+                                SELECT DISTINCT A2.artist_id, P.playlist_id FROM artist A2
+                                    INNER JOIN wrote W2 ON A2.artist_id = W2.artist_id
+                                    INNER JOIN in_playlist P ON W2.song_id = P.song_id
+                            ) AS A2
+                            GROUP BY A2.artist_id
+                            ORDER BY count(A2.artist_id) DESC
+                        ) AS playlist_songs LIMIT 20
+                    ) inPlay ON A.artist_id = inPlay.artist_id
+            ) AS most_pop_artists
+            ORDER BY in_num_of_playlists DESC;
+        `
+    )
+    .then(results => { response.status(200).json(results.rows) })
+    .catch(error => {
+        console.log(error.detail);
+        response.status(400).json(error.detail);
+    })
+}
+
+module.exports = {
+    getTop20Songs,
+    getTop20Artists
+}
--- endpoints/song.js
+++ endpoints/song.js
@@ -2,11 +2,14 @@ var express = require('express')
 var router = express.Router()
 
 var queries = require('../endpointImpl/songQueries')
+var popQueries = require('../endpointImpl/mostPopQueries')
 
 router.get('/:text', queries.searchText);
 
 // Future endpoints:
 //    /popularity             GET      getPopular
 //    /mostPlaylist ????      GET       inMostPlaylists
+router.get('/:');
+router.get('/:');
 
 module.exports = router
