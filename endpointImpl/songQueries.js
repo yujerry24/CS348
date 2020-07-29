0bfd05ed40ce68fa0c77d2b5d16135cae626c810
Hannah Li <hannahli2010@gmail.com>
2020-07-29 18:02:17 -0400

    Add more return attributes to new song search, Remove old one

--- endpointImpl/songQueries.js
+++ endpointImpl/songQueries.js
@@ -2,69 +2,8 @@ const { pool } = require('../dbPool');
 const { formatSongs } = require('./utils');
 
 /*
- * /song/search/:userId/:text
- *
- * Returns: {
- *   'song_id': {
- *      song_name: string,
- *      artist_name: string,
- *      album_name: string,
- *      video_duration: number
- *   },
- *   ...
- * }
- */
-const searchText = (req, response) => {
-  pool
-    .query(
-      `
-        SELECT S.song_id, S.name as song_name, AR.name as artist_name, AL.name as album_name, video_duration, video_id,
-        ( S.song_id IN (
-          SELECT song_id 
-          FROM in_playlist
-          WHERE playlist_id=$2::text)
-          ) as isFavourite
-        FROM song S
-          INNER JOIN wrote W ON S.song_id = W.song_id
-          INNER JOIN artist AR ON W.artist_id = AR.artist_id
-          INNER JOIN album AL ON AL.album_id = S.album_id
-        WHERE (
-          S.song_id IN (
-            SELECT song_id 
-            FROM song
-            WHERE LOWER(song.name) LIKE LOWER($1::text)
-          )
-        ) OR (
-          S.song_id IN (
-            SELECT song_id 
-            FROM wrote INNER JOIN artist ON wrote.artist_id = artist.artist_id 
-            WHERE LOWER(artist.name) LIKE LOWER($1::text)
-          )
-        ) OR (
-          S.song_id IN (
-            SELECT song_id 
-            FROM song INNER JOIN album ON song.album_id = album.album_id 
-            WHERE LOWER(album.name) LIKE LOWER($1::text)
-          )
-        )
-        LIMIT 30
-      `,
-      [`%${req.params.text}%`, `${req.params.userId}-liked-songs`]
-    )
-    .then(results => {
-      const formatData = formatSongs(results);
-      response.status(200).json(formatData);
-    })
-    .catch(error => {
-      console.log(error);
-      response.status(400).send(`An error occurred during the query`);
-    });
-};
-
-
-/*
  * GET
- * /song/minisearch/:text
+ * /song/search/:userId/:text
  *
  * body: {
  *   limit: [int], // optional maximum number of results to return
@@ -73,15 +12,22 @@ const searchText = (req, response) => {
  *   'song_id': {
  *      song_name: string
  *      artist_name: string
- *      album_name: string
+ *      album_name: string,
+ *      video_duration: number,
+ *      video_id: string,
+ *      isfavourite: boolean
  *   },
  *   ...
  * }
  */
 const searchSong = (req, response) => {
-  let query =       
-  `
-  SELECT S.song_id, S.name as song_name, AR.name as artist_name, AL.name as album_name
+  let query = `
+  SELECT S.song_id, S.name as song_name, AR.name as artist_name, AL.name as album_name, video_duration, video_id,
+  ( S.song_id IN (
+    SELECT song_id 
+    FROM in_playlist
+    WHERE playlist_id=$2::text)
+    ) as isFavourite
   FROM song S
     INNER JOIN wrote W ON S.song_id = W.song_id
     INNER JOIN artist AR ON W.artist_id = AR.artist_id
@@ -95,13 +41,10 @@ const searchSong = (req, response) => {
   ) 
   `;
   if (req.body.limit && req.body.limit > 0) {
-    query += ` LIMIT ${req.body.limit}`
+    query += ` LIMIT ${req.body.limit}`;
   }
   pool
-    .query(
-      query,
-      [`%${req.params.text}%`]
-    )
+    .query(query, [`%${req.params.text}%`, `${req.params.userId}-liked-songs`])
     .then(results => {
       const formatData = formatSongs(results);
       response.status(200).json(formatData);
@@ -113,6 +56,5 @@ const searchSong = (req, response) => {
 };
 
 module.exports = {
-  searchText,
-  searchSong
+  searchSong,
 };
--- endpoints/song.js
+++ endpoints/song.js
@@ -4,13 +4,8 @@ var router = express.Router();
 var queries = require('../endpointImpl/songQueries');
 var popQueries = require('../endpointImpl/mostPopQueries');
 
-router.get('/search/:userId/:text', queries.searchText);
-router.get('/searchMore/:text', queries.searchText);
-router.post('/search/:text', queries.searchSong);
+router.post('/search/:userId/:text', queries.searchSong);
 
-// Future endpoints:
-//    /popularity             GET       getPopular
-//    /mostPlaylist ????      GET       inMostPlaylists
 router.get('/popularSongs', popQueries.getTop20Songs);
 router.get('/popularArtists', popQueries.getTop20Artists);
 
