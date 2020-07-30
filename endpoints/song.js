e2c9bd30f1104c712d326e0cf6b8e79f7cd57577
Hannah Li <hannahli2010@gmail.com>
2020-07-29 21:05:07 -0400

    Add isfavourite to top 20 songs query

--- endpoints/song.js
+++ endpoints/song.js
@@ -6,7 +6,7 @@ var popQueries = require('../endpointImpl/mostPopQueries');
 
 router.post('/search/:userId/:text', queries.searchSong);
 
-router.get('/popularSongs', popQueries.getTop20Songs);
+router.get('/popularSongs/:userId', popQueries.getTop20Songs);
 router.get('/popularArtists', popQueries.getTop20Artists);
 
 module.exports = router;
