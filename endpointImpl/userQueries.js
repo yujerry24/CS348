6f989151c9e68e785ca14824cbddc63f930cf53e
yujerry24 <yujerry24@gmail.com>
2020-08-03 20:53:14 -0400

    Create liked playlist on user creation

--- endpointImpl/userQueries.js
+++ endpointImpl/userQueries.js
@@ -29,6 +29,37 @@ const findUser = (req, response) => {
   POST
   /user/new
 */
+const deletePlaylist = (req, response) => {
+  let output = '';
+  pool
+    .query(
+      `
+        DELETE FROM in_playlist WHERE playlist_id = $1::text;
+      `,
+      [req.params.playlistId]
+    )
+    .then(() => {
+      output += 'Deleted all songs from the playlist.\n';
+    })
+    .then(() => {
+      pool
+        .query(
+          `
+          DELETE FROM playlist WHERE playlist_id = $1::text
+        `,
+          [req.params.playlistId]
+        )
+        .then(() => {
+          output += 'Deleted the playlist.\n';
+          response.status(200).send(output);
+        });
+    })
+    .catch(error => {
+      console.log(error);
+      response.status(400).json(error.detail);
+    });
+};
+
 
 const createUser = (req, response) => {
   pool.query(
@@ -37,6 +68,19 @@ const createUser = (req, response) => {
     `,
     [req.body.userId]
   )
+  .then(() => {
+    pool
+        .query(
+          `
+          INSERT INTO playlist VALUES ($1::text, $2::text, 0, $3::text)
+        `,
+          [req.body.userId+'-liked-songs', 'My Liked Songs', req.body.userId]
+        )
+        .then(() => {
+          output += 'Deleted the playlist.\n';
+          response.status(200).send(output);
+        });
+  })
   .then(() => {
     response.status(200).json(`Created new user "${req.body.userId}"`);
   })
